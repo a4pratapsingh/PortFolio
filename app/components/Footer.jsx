8760c8f537679ca6ec02476d5042bb543a0ee8ee
impratap <ankitpratapsingh333@gmail.com>
2026-02-23 13:51:35 +0530

    update

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -16,7 +16,7 @@ function Footer({isDarkMode}) {
 
         <div className='text-center sm:flex items-center justify-between border-t
         border-gray-400 mx-[10%] mt-12 py-6'>
-            <p>© 2025 Ankit Singh. All rights reserved.</p>
+            <p>© 2026 Ankit Singh. All rights reserved.</p>
             <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
                 <li><a target='_blank' href="https://github.com/impratap">GitHub</a></li>
             <li><a target='_blank' href="https://www.linkedin.com/in/a4pratapsingh/">Linkedin</a></li>
